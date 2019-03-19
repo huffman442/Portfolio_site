62bb190522bded0774dcc9200fd8ac88a0e1f2f4
Michael Huffman <huffman442@yahoo.com>
2019-03-19 15:32:39 -0400

    add tags to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,22 @@
 class Article < ApplicationRecord
+    has_many :taggings
+    has_many :tags, through: :taggings
+
     validates :title, presence: true,
                       length: { minimum: 5 }
+    validates :picture, presence: true
+
+    def self.tagged_with(body)
+        Tag.find_by!(body: body).articles
+    end
+
+    def tag_list
+        tags.map(&:body).join(', ')
+    end
+    
+      def tag_list=(names)
+        self.tags = names.split(',').map do |n|
+          Tag.where(body: n.strip).first_or_create!
+        end
+    end
 end
