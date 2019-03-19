7eecf8380d34a6ad1ca9e56d39fc013c6d007a43
Michael Huffman <huffman442@yahoo.com>
2019-03-19 15:34:18 -0400

    Implement a better blog preview

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   def index
     @articles = Article.all
+    @articles_displayed = Array.new
+    @articles.reverse.each do |article|
+      @articles_displayed << article
+      break if @articles_displayed.length == 3
+    end
   end
 
   def resume
